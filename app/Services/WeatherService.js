d4b76010a2623439056f8308a686393e62435fb5
Savannah Temple <savtemple@gmail.com>
2023-01-28 15:53:55 -0700

    more weather data added

--- app/Services/WeatherService.js
+++ app/Services/WeatherService.js
@@ -5,6 +5,7 @@ import { sandboxApi } from "./AxiosService.js"
 class WeatherService {
     async getWeather() {
         const res = await sandboxApi.get('api/weather')
+        // console.log(res);
         appState.weather = new Weather(res.data)
         console.log('weather', appState.weather);
     }

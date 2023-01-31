42d1acf4cf515ea0586d904f3beb48fec07f7ee1
SamanthaDison <samnicdis@gmail.com>
2023-01-31 03:22:09 -0700

    ok now styling is finished

--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -10,10 +10,10 @@ export class Todo {
 
   get Template() {
     return ` <div class="d-flex justify-content-between align-items-center todo-item my-2">
-                <i class=" fs-4 hoverable mdi mdi-checkbox-${this.completed ? 'marked' : 'blank-outline'}"
+                <i title="Complete task" class=" fs-4 hoverable mdi mdi-checkbox-${this.completed ? 'marked' : 'blank-outline'}"
                   onclick="app.todosController.completeTodo('${this.id}')"></i>
                 <p class="px-2 m-0 VT fs-3">${this.description}</p>
-          <i class="fs-4 hoverable mdi mdi-alpha-x-box-outline" onclick="app.todosController.deleteTodo('${this.id}')"></i>
+          <i title="Remove task" class="fs-4 hoverable mdi mdi-alpha-x-box-outline" onclick="app.todosController.deleteTodo('${this.id}')"></i>
               </div>
         `
   }
--- app/Models/Vibe.js
+++ app/Models/Vibe.js
@@ -16,10 +16,10 @@ export class Vibe {
         return `
     <div class="pe-4 text-shadow">
       <h4 class="pt-3 ps-3"> ${this.name}</h4>
-      <p class="w-100 text-end"><b>plays ${this.plays}</b></p>
+      <p class="ps-4"><b>plays: ${this.plays}</b></p>
     </div>
     <div class="text-end p-3">
-    <iframe id="sound-cloud" class="" width="250" height="250" scrolling="no" frameborder="no" allow="autoplay"
+    <iframe id="sound-cloud" class="w-100"  height="250" scrolling="no" frameborder="no" allow="autoplay"
     src="${this.musicSource}"></iframe>
     </div>
     `

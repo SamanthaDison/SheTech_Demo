c3dbed4bdb56b5e492036284c4c3ce75fd6c35b7
SamanthaDison <samnicdis@gmail.com>
2023-01-31 01:14:16 -0700

    weather comp styled

--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -3,7 +3,7 @@ export class Weather {
         this.name = data.name
         this.description = data.weather[0].description
         this.humidity = data.main.humidity
-        this.fahrenheit = ((data.main.temp - 273.15) * 9 / 5 + 32).toFixed(2)
+        this.fahrenheit = ((data.main.temp - 273.15) * 9 / 5 + 32).toFixed(0)
         this.celsius = (data.main.temp - 273.15).toFixed(2)
         this.icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
         this.isFahrenheit = true
@@ -13,18 +13,20 @@ export class Weather {
 
     get WeatherTemplate() {
         return `    
-        <div class="row">
-            <p class="m-0">${this.name}</p>
-            <p>${this.description}</p>
-        </div>
-        <div class="row">
-            <div class="col-6 d-flex">
+        <div class="weather-header d-flex justify-content-around press-start text-white">
+        
+            <p class="m-0 p-0">${this.name}</p>
+            <p class="m-0">||</p>
+            <p class="m-0 p-0">${this.description}</p>
+       </div>
+        <div class="weather-body d-flex press-start justify-content-between align-items-center p-1">
+            <div class=" d-flex">
                 <img src="${this.icon}" alt="">
-                <p class="mb-0 mt-3">${this.isFahrenheit ? this.fahrenheit : this.celsius} °F</p>
+                <p class="m-0 mt-3 fs-temp">${this.isFahrenheit ? this.fahrenheit : this.celsius}°F</p>
             </div>
-            <div class="col-6">
-                <p> <span>Feels Like</span> ${this.fahrenheitFeelsLike} °F</p>
-                <p> <span>Humidity</span> ${this.humidity}</p>
+            <div class="">
+                <p class="m-0"> <span>Feels Like</span>: ${this.fahrenheitFeelsLike}°F</p>
+                <p class="m-0"> <span>Humidity</span>: ${this.humidity}</p>
             </div>
         </div>
         `
